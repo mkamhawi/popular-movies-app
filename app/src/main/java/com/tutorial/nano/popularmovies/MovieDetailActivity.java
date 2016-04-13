55b93fc027d948f6a21057dadb31e45194e0a43d
Mohamed elKamhawi <mkamhawie@gmail.com>
2016-04-13 12:26:02 +0200

    Refactoring: Imbed the fragments in their respective activities in the layout xml rather than imbedding them dynamically.

--- app/src/main/java/com/tutorial/nano/popularmovies/MovieDetailActivity.java
+++ app/src/main/java/com/tutorial/nano/popularmovies/MovieDetailActivity.java
@@ -9,10 +9,5 @@ public class MovieDetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie_detail);
-        if(savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                    .add(R.id.movie_detail_container, new MovieDetailFragment())
-                    .commit();
-        }
     }
 }
